19b83114058f4a44eef02b313cd205c36373af0b
Mahendra Dhayal <mahendra.kum.dhayal@hpe.com>
2017-04-14 16:40:23 +0530

    First checkin

---
+++ stats-platform/stats-platfrom-test/src/main/java/org/stats/test/MyJava.java
@@ -0,0 +1,12 @@
+package org.stats.test;
+
+/**
+ * Created by dhayal on 4/14/2017.
+ */
+public class MyJava {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello India !");
+    }
+}
